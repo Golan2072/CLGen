70f27f95e97aae60ac03994690fd9073c5ee25c4
Jo Jaquinta <jo_grant@us.ibm.com>
2021-02-09 16:32:29 -0500

    API access. Swagger return.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, Response
 from werkzeug.utils import redirect
+import json
 
 from app import app
 import clgen_lib
@@ -24,3 +25,20 @@ def chargen():
     else:
         pass
 
+
+@app.route("/api")
+def apiRoot():
+    with open('./api.swagger', 'r') as f:
+        data = f.read()
+    return Response(data, mimetype='application/x-yml')
+
+@app.route("/api/chargen")
+def apiChargen():
+    death = True
+    darg = request.args.get("death", None)
+    if darg is not None:
+        if darg.lower() == "false":
+            death = False
+    print("death="+str(death))
+    character = clgen_lib.Character(death)
+    return Response(json.dumps(character.__dict__), mimetype='application/json')
