14301c54c3e9d254f37fe2d24322992f0376fe63
Omer Golan Joel <golan2072@gmail.com>
2021-02-10 18:39:12 +0200

    v1.3

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, Response
 from werkzeug.utils import redirect
+import json
 
 from app import app
 import clgen_lib
@@ -18,9 +19,27 @@ def chargen():
                            upp_string=character.upp_string, age=character.age, status=character.status,
                            career=character.career, rank_name=character.rank_name, terms=character.terms,
                            credits=character.cash, possessions=character.possessions_string,
-                           skills=character.skill_string)
+                           skills=character.skill_string,
+                           deathDefault=death)
     if request.args.get('generate') == 'Generate':
         return redirect(url_for('chargen', Death=death))
     else:
         pass
 
+
+@app.route("/api")
+def apiRoot():
+    with open('./api.swagger', 'r') as f:
+        data = f.read()
+    return Response(data, mimetype='application/x-yml')
+
+@app.route("/api/chargen")
+def apiChargen():
+    death = True
+    darg = request.args.get("death", None)
+    if darg is not None:
+        if darg.lower() == "false":
+            death = False
+    print("death="+str(death))
+    character = clgen_lib.Character(death)
+    return Response(json.dumps(character.__dict__), mimetype='application/json')
